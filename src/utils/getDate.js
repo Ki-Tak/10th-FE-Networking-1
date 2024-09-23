5dd59a8a2eab48bacfc6334f30451279647cc681
Ki-Tak <lee08tain@gmail.com>
2024-09-23 13:13:38 +0900

    파일 구성 및 헤더 구조 작성 중

---
+++ src/utils/getDate.js
@@ -0,0 +1,12 @@
+function getDateStr() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더함
+    const date = String(today.getDate()).padStart(2, '0');
+    // 요일 배열
+    const arr = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+    const dayOfWeek = arr[today.getDay()]; // 0-6 (일-토)
+
+    return `${year}.${month}.${date} ${dayOfWeek}`;
+}
+export default getDateStr;
