693f8b250a502fa5c7f0a28aed623bf4342b9efc
Ki-Tak <lee08tain@gmail.com>
2024-10-07 21:04:28 +0900

    롤링 애니메이션 구현

--- src/components/HeadLine.js
+++ src/components/HeadLine.js
@@ -19,19 +19,41 @@ async function HeadLine() {
     } catch (error) {
         console.error('Error fetching headline data:', error);
     }
-    const updateHeadLine = () => {  // 헤드라인 업데이트
-        document.querySelector('article').innerHTML = render();
+    const updateLeft = () => { // 왼쪽 헤드라인 업데이트 하는 함수 
+        // 왼쪽 헤드라인 값을 가져와서 headline 클래스를 추가하여 애니메이션을 줌
+        // 애니메이션과 함께 텍스트 변경
+        // 500ms 이후 (애니메이션 시간) headline 클래스 삭제 
+        const leftHeadLine = document.getElementById('left');
+        leftHeadLine.classList.add('headline');
+        leftHeadLine.innerHTML = `
+        <h1>${headLineData.newHeadLine1[leftIndex].journer}</h1>
+        <h2>${headLineData.newHeadLine1[leftIndex].content}</h2>
+    `;
+        setTimeout(() => {
+            leftHeadLine.classList.remove('headline');
+        }, 500);
+    }
+    const updateRight = () => { // 오른쪽 헤드라인 업데이트하는 함수
+        const rightHeadLine = document.getElementById('right');
+        rightHeadLine.classList.add('headline');
+        rightHeadLine.innerHTML = `
+            <h1>${headLineData.newHeadLine2[rightIndex].journer}</h1>
+            <h2>${headLineData.newHeadLine2[rightIndex].content}</h2>
+        `;
+        setTimeout(() => {
+            rightHeadLine.classList.remove('headline');
+        }, 500);
     }
     const rollHeadLine = () => {  // 헤드라인 롤링
         headLineTimer = setTimeout(() => {
             leftIndex = (leftIndex + 1) % headLineData.newHeadLine1.length;
-            updateHeadLine();
+            updateLeft();
             setTimeout(() => {
                 rightIndex = (rightIndex + 1) % headLineData.newHeadLine2.length;
-                updateHeadLine();
+                updateRight();
             }, 1000);
             rollHeadLine(); // 재귀호출 
-        }, 5000);
+        }, 4500); // 애니메이션 시간 생각하여 500 줄임 
     };
 
     const startRoll = () => { // 타이머 중복호출 방지
@@ -52,15 +74,16 @@ async function HeadLine() {
     const render = () => {
         const element = ` 
         <article>
-            <div>
+            <div id='left'>
             <h1>${headLineData.newHeadLine1[leftIndex].journer}</h1>
             <h2>${headLineData.newHeadLine1[leftIndex].content}</h2>
             </div>
-            <div>
+            <div id ='right'>
             <h1>${headLineData.newHeadLine2[rightIndex].journer}</h1>
             <h2>${headLineData.newHeadLine2[rightIndex].content}</h2>
             </div>
-        </article>`
+        </article>
+        `
         // DOM에 요소 삽입 후 이벤트 리스너 추가
         setTimeout(() => {
             addHeadLineEventListeners();
